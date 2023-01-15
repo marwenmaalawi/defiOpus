36e9a32bbba0b225fd9a3ca5574ade9a37899b6b
marwenmaalawi <mohamedmarwen.maalawi@sprit.tn>
2023-01-15 18:43:25 +0100

    edit app.controller

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,13 +1,10 @@
 import { Controller, ForbiddenException, Get, Param } from '@nestjs/common';
 import { AppService } from './app.service';
-import { HttpService } from '@nestjs/axios';
-import { SearchService } from './search/search.service';
-import { response } from 'express';
-import { ResultService } from './search/result.service';
+
 const axios = require("axios");
 @Controller('api')
 export class AppController {
-  constructor(private readonly appService: AppService, private readonly searchService:SearchService,private readonly resultService:ResultService) {}
+  constructor(private readonly appService: AppService) {}
  
   @Get()
   getHello(): string {
@@ -16,19 +13,8 @@ export class AppController {
   @Get('getsearchByterm/:term')
   async getsrachByTerm(@Param('term') term:string){
 
-      var e= await this.searchService.searchfindbyterm(term)
-      if(e==null){
-       
-        const response = await axios.get('https://api.unsplash.com/search/photos?query='+term+'&client_id=LlEdPT15nX3QmHu2tXR8VNFETAIzWMK68mIO16YlVsY');
-        let r=response.data.results;
-      let s= await this.searchService.searchCreate({"term":term})
-        for(let i=0;i<r.length;i++){
-       await this.resultService.resultCreate({"created_at":r[i].created_at,"url":r[i].urls.full,"username":r[i].user.username,"profile_image":r[i].user.profile_image.large,"search":s.search})
-        }     
-       e= await this.searchService.searchfindbyterm(term)
-           
-    }
-    return e.results
+     return this.appService.getsrachByTerm(term);
+
     }
   
   
--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -1,8 +1,31 @@
 import { Injectable } from '@nestjs/common';
+import { SearchService } from './search/search.service';
+import { ResultService } from './search/result.service';
+const axios = require("axios");
 
 @Injectable()
+
 export class AppService {
+  constructor(private readonly searchService:SearchService,private readonly resultService:ResultService) {
+
+  }
   getHello(): string {
     return 'Hello World!';
+  }
+  async getsrachByTerm(term:string){
+
+    var e= await this.searchService.searchfindbyterm(term)
+    if(e==null){
+     
+      const response = await axios.get('https://api.unsplash.com/search/photos?query='+term+'&client_id=LlEdPT15nX3QmHu2tXR8VNFETAIzWMK68mIO16YlVsY');
+      let r=response.data.results;
+    let s= await this.searchService.searchCreate({"term":term})
+      for(let i=0;i<r.length;i++){
+     await this.resultService.resultCreate({"created_at":r[i].created_at,"url":r[i].urls.full,"username":r[i].user.username,"profile_image":r[i].user.profile_image.large,"search":s.search})
+      }     
+     e= await this.searchService.searchfindbyterm(term)
+         
+  }
+  return e.results
   }
 }
